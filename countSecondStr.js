b1e103425c6489a73f59c5db660ab6dc912afae8
Nguyễn Huỳnh Trường An <35690285+huynhtruongan@users.noreply.github.com>
2019-12-01 23:39:12 +0700

    Update countSecondStr.js

--- countSecondStr.js
+++ countSecondStr.js
@@ -1,22 +1,20 @@
-const str = "range represented by a node is completely inside the given range";
+const string = "range represented by a node is completely inside the given range";
 let re = /\S+/g;
 
-async function countStr(str){
-  
-    let count = 0;
-    const arr = str.match(re);
-    const result =  await Promise.all(
-      arr.map(async function(char){
-        if(await char.match(/[aeoui]/g)!=null&&char.match(/[aeoui]/g).length>=2){
-            await count++; 
-          }
-         
+
+function countVowels(string){
+  //your code here;
+  let arr = [],count = 0;
+    if(!!string){
+      arr = string.match(/\S+/g);
+      arr.map(char=>{
+       if(char.match(/[aeoui]/g)!=null && char.match(/[aeoui]/ig).length >= 2) count ++;
       })
-      
-    )
-    // console.log(count)
-    // FIXED: return count
-    return result
+    }
+
+  return count
 }
 
-countStr(str)
+Test.assertEquals(countVowels("my first kata"),1, "The string has 1 word containing a second vowels");
+Test.assertEquals(countVowels("Once upon a time"),3, "Should account for upper case elements");
+Test.assertEquals(countVowels(""),0, "Empty string");
